4cbac1df9b0edfcbe71bf718f1d4bbed737128bd
lindomar <lindomarbatistao@gmail.com>
2026-08-24 11:22:55 -0300

    Filtros especiais

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
@@ -16,9 +17,11 @@ class UsuarioViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = UsuarioFilter
     
+    
     @action(
         detail = False,
-        methods = ["get"]
+        methods = ["get"],
+        permission_classes = [IsAdminUser]
     )
     def nomes(self, request):
         usuarios = Usuario.objects.all()
@@ -34,6 +37,19 @@ class ImovelViewSet(ModelViewSet):
     
     filter_backends = [DjangoFilterBackend]
     filterset_class = ImovelFilter
+    
+    @action(
+            detail = False,
+            methods = ["get"],
+            permission_classes = [IsAdminUser]
+        )
+    def titulos(self, request):
+        imoveis = Imovel.objects.all()
+        dados = [
+            imovel.titulo
+            for imovel in imoveis
+        ]
+        return Response(dados)
 
 class ContratoViewSet(ModelViewSet):
     queryset = Contrato.objects.all()
@@ -41,6 +57,23 @@ class ContratoViewSet(ModelViewSet):
     
     filter_backends = [DjangoFilterBackend]
     filterset_class = ContratoFilter
+    
+    @action(
+            detail = False,
+            methods = ["get"],
+            permission_classes = [IsAdminUser]
+        )
+    def usuarios(self, request):
+        contratos = Contrato.objects.all()
+        dados = [
+            {
+                "numero": contrato.id,
+                "locador": contrato.locador.get_full_name(),
+                "locatario": contrato.locatario.get_full_name()
+            }
+            for contrato in contratos
+        ]
+        return Response(dados)
 
 class PagamentoViewSet(ModelViewSet):
     queryset = Pagamento.objects.all()
@@ -48,3 +81,21 @@ class PagamentoViewSet(ModelViewSet):
     
     filter_backends = [DjangoFilterBackend]
     filterset_class = PagamentoFilter
+    
+    @action(
+                detail = False,
+                methods = ["get"],
+                permission_classes = [IsAdminUser]
+            )
+    def locatarios(self, request):
+        pagamentos = Pagamento.objects.all()
+        
+        dados = [
+            {
+                "locatario": pagamento.contrato.locatario.get_full_name(),
+                "valor": pagamento.valor,
+                "data_pagamento": pagamento.data_pagamento
+            }
+            for pagamento in pagamentos
+        ]
+        return Response(dados)
