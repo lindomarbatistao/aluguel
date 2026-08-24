6c592853c281d12898b98f62a9c467a66e7b168e
lindomar <lindomarbatistao@gmail.com>
2026-08-24 08:16:29 -0300

    XX

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views_viewset import *
+from .views import *
 
 router = DefaultRouter()
 
@@ -16,8 +16,8 @@ router.register("pagamentos", PagamentoViewSet, basename="pagamentos")
 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path("viewset/", include(router.urls)),
 ]
