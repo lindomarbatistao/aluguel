8c8227f9424c5a16200175a96e34ec652816d913
lindomar <lindomarbatistao@gmail.com>
2026-08-27 08:13:04 -0300

    Conserto da env

--- alugueis/settings.py
+++ alugueis/settings.py
@@ -22,9 +22,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
