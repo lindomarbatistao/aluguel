05ab178d09776fb5caa9bf650d98c147462fe71d
lindomar <lindomarbatistao@gmail.com>
2026-08-27 10:16:56 -0300

    Gráficos

--- api/views.py
+++ api/views.py
@@ -10,6 +10,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 
+import matplotlib.pyplot as plt
+from django.http import HttpResponse
+from django.db.models import Count
+from io import BytesIO
+
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
@@ -38,11 +43,7 @@ class ImovelViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = ImovelFilter
     
-    @action(
-            detail = False,
-            methods = ["get"],
-            permission_classes = [IsAdminUser]
-        )
+    @action(detail = False, methods = ["get"],permission_classes = [IsAdminUser])
     def titulos(self, request):
         imoveis = Imovel.objects.all()
         dados = [
@@ -50,7 +51,67 @@ class ImovelViewSet(ModelViewSet):
             for imovel in imoveis
         ]
         return Response(dados)
+    
+    @action(detail=False, methods=['GET'])
+    def grafico(self, request):
+        dados = Imovel.objects.values("tipo").annotate(total=Count("id"))
+        
+        tipos = [item["tipo"] for item in dados]
+        totais = [item["total"] for item in dados]
+        
+        plt.bar(tipos, totais)
+        plt.title("Imobiliaria LogiTude")
+        plt.xlabel("Tipos")
+        plt.ylabel("Qde")
+        
+        imagem = BytesIO()
+        plt.savefig(imagem, format="png")
+        plt.close()
+        imagem.seek(0)
+        
+        return HttpResponse(imagem, content_type="image/png")
+    
+    @action(detail=False, methods=['GET'])
+    def grafic(self, request):
+        dados = Imovel.objects.values("tipo").annotate(total=Count("id"))
+        
+        fig, ax = plt.subplots()
 
+        titulos = [item["tipo"] for item in dados]
+        counts = [item["total"] for item in dados]
+        bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange']
+
+        ax.bar(titulos, counts, color=bar_colors)
+
+        ax.set_ylabel('Títulos')
+        ax.set_title('Imobiliária do Morpheu')
+
+        imagem = BytesIO()
+        plt.savefig(imagem, format="png")
+        plt.close()
+        imagem.seek(0)
+        
+        return HttpResponse(imagem, content_type="image/png")
+
+    @action(detail=False, methods=['GET'])
+    def pie(self, request):
+        dados = Imovel.objects.values("tipo").annotate(total=Count("id"))
+
+        data = [item["total"] for item in dados]
+        labels = [item["tipo"] for item in dados]
+
+        fig, ax = plt.subplots()
+        pie = ax.pie(data)
+        ax.pie_label(pie, labels)
+        ax.pie_label(pie, '\n\n{frac:.1%}')
+
+        imagem = BytesIO()
+        plt.savefig(imagem, format="png")
+        plt.close()
+        imagem.seek(0)
+        
+        return HttpResponse(imagem, content_type="image/png")
+        
 class ContratoViewSet(ModelViewSet):
     queryset = Contrato.objects.all()
     serializer_class = ContratoSerializer
